4fe1b19cade70bd528b983ba7aeb85d46f11605e
Sebastian Key <sebastianwkey@gmail.com>
2015-02-11 19:16:32 +1100

    properly removed console.log message

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('.scroll-down').click(function(e) {
-      console.log('link pressed')
       $('html,body').animate({scrollTop: $(document).height()}, 400);
   });
 });
